801eccaeae23b14fdbdf4644eeca7791331104a6
Sharanya Mitra <sharanyamitra28@gmail.com>
2022-06-15 22:45:23 +0530

    adding search menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { render } from "@testing-library/react";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monster: [],
+    };
+  }
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>response.json())
+    .then(
+      (users)=> 
+      this.setState(
+        ()=>{return {monster:users}}
+        )
+      )
+  }
+  render() {
+    return (
+      <div className="App">
+      <input className="Search-box"type="search"placeholder="Mosters name" onClick={(event)=>{
+        console.log(event.target.value);
+        
+      }}/>
+        {this.state.monster.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
