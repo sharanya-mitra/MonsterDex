e241e7471cda6937bc0f8a4081001124b510324f
Sharanya Mitra <sharanyamitra28@gmail.com>
2022-06-16 06:42:23 +0530

    doing changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { render } from "@testing-library/react";
 import { Component } from "react";
-
 class App extends Component {
   constructor() {
     super();
@@ -10,31 +9,43 @@ class App extends Component {
       monster: [],
     };
   }
-  componentDidMount()
-  {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response)=>response.json())
-    .then(
-      (users)=> 
-      this.setState(
-        ()=>{return {monster:users}}
-        )
-      )
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monster: users };
+        })
+      );
   }
   render() {
     return (
       <div className="App">
-      <input className="Search-box"type="search"placeholder="Mosters name" onClick={(event)=>{
-        console.log(event.target.value);
-        
-      }}/>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchstring = event.target.value.toLocaleLowerCase();
+            const filteredmonster = this.state.monster.filter((monsters)=>{
+              return monsters.name.toLocaleLowerCase().includes(searchstring);
+            });
+            this.setState(()=>{
+              return {monster:filteredmonster};
+            });
+            }}
+        />
         {this.state.monster.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
   }
-  
 }
 
 export default App;
