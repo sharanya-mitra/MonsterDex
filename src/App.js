0295760828496c61def81fa3530ee2abdc37cbcb
Sharanya Mitra <sharanyamitra28@gmail.com>
2022-06-21 14:38:18 +0530

    done search bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 // import { render } from "@testing-library/react";
 import { Component } from "react";
 class App extends Component {
-  
   constructor() {
     super();
     this.state = {
       monster: [],
+      searchFild:''
     };
   }
   componentDidMount() {
@@ -19,24 +19,25 @@ class App extends Component {
         })
       );
   }
-
-
   render() {
-
+    const filteredmonster = this.state.monster.filter((monsters)=>{
+      return monsters.name.toLocaleLowerCase().includes(this.state.searchFild);
+    });
     return (
       <div className="App">
-        <input className="search-box" type="search" placeholder="Search Monsters"
-          onChange={(event) => {console.log(event.target.value);
-            const searchstring = event.target.value.toLocaleLowerCase();
-            const filteredmonster = this.state.monster.filter((monsters)=>{
-              return monsters.name.toLocaleLowerCase().includes(searchstring);
-            });
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchFild = event.target.value.toLocaleLowerCase();
             this.setState(()=>{
-              return {monster:filteredmonster};
+              return {searchFild};
             });
             }}
         />
-        {this.state.monster.map((monster) => {
+        {filteredmonster.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
